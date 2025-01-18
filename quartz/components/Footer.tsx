02acf870cf7584b95fc031099fdeb36f25543ee0
camroybal <camroybal@gmail.com>
2025-01-17 18:51:40 -0700

    Colorway tweak and layout

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,8 +13,8 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
+        <p> 
+          {i18n(cfg.locale).components.footer.createdWith}{" "} 
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
